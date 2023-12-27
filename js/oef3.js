6b8c4036993b37d26886b27f25e8167b3b46bb70
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-27 14:55:59 +0100

    Update oef3.js

--- js/oef3.js
+++ js/oef3.js
@@ -1,2 +1,12 @@
 console.log('script loaded');
 
+let btn = document.getElementById('btn1');
+let div = document.getElementById('container');
+
+btn.addEventListener('click', function () {
+    console.log('Yes! Het werkt');
+    let p = document.createElement('p');
+    let txt = document.createTextNode('Yes! Het werkt');
+    p.appendChild(txt);
+    div.appendChild(p);
+});
