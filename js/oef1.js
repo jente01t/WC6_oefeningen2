7af3d16d8cb37d3c0eb22b1539ca871be2d8c63b
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-27 14:30:44 +0100

    Update oef1.js

--- js/oef1.js
+++ js/oef1.js
@@ -5,7 +5,11 @@ let titel = document.querySelector('#titel2');
 console.log(titel);
 
 // let paragrafen = document.getElementsByTagName('p');
-
+let paragrafen = document.querySelectorAll('p');
 for (let i = 0; i < paragrafen.length; i++) {
     console.log(paragrafen[i]);
 }
+
+// let speciaal = document.getElementsByClassName('speciaal');
+let speciaal = document.querySelectorAll('.speciaal');
+console.log(speciaal[0]);
