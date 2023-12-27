73b37606873a02892229170ec0b6cf669d9f633a
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-27 16:01:09 +0100

    update main

--- js/oef5.js
+++ js/oef5.js
@@ -1,15 +1,22 @@
-console.log('script loaded');
+console.log('scipt loaded');
 
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 let letters = document.getElementById('letters');
+let output = document.getElementById('output');
+
+let chosen = output.innerHTML;
 
 for (let i = 0; i < alphabet.length; i++) {
     let letter = alphabet[i];
     let div = document.createElement('div');
     div.classList.add('letter');
     div.id = letter
-    div.innerHTML = letter
-
     letters.appendChild(div);
+    div.innerHTML = letter;
+
+    div.addEventListener('click', function () {
+        output.innerHTML = chosen += ' ' + letter
+        div.classList.add('done');
+    })
 }
