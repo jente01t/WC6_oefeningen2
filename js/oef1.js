960b66a520bfc057c9be809c42213432dfab3b0f
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-27 14:28:08 +0100

    update main

---
+++ js/oef1.js
@@ -0,0 +1,11 @@
+console.log('Script loaded')
+
+// let titel = document.getElementById('titel2');
+let titel = document.querySelector('#titel2');
+console.log(titel);
+
+// let paragrafen = document.getElementsByTagName('p');
+
+for (let i = 0; i < paragrafen.length; i++) {
+    console.log(paragrafen[i]);
+}
